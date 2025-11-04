05a2e9108ae39b53b4b763e09e2d945b353c898a
Michal Czycza <s28517@pjwstk.edu.pl>
2025-11-04 14:13:41 +0100

    initialize

---
+++ pages/auth/login.js
@@ -0,0 +1,12 @@
+// /pages/auth/login.js
+import AuthForm from '../../modules/auth/AuthForm';
+
+const LoginPage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <AuthForm />
+        </div>
+    );
+};
+
+export default LoginPage;
